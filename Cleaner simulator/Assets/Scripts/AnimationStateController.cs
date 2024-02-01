fdb26129a9ffea449fd5d01aa40d202e3e5c00bd
pascal_pro_2007 <m.brednev89@gmail.com>
2024-02-01 12:52:44 +0500

    сделал алгоритм анимаций в отдельном скрипте, надо допилить приседание во время ходьбы или бега

--- Cleaner simulator/Assets/Scripts/AnimationStateController.cs
+++ Cleaner simulator/Assets/Scripts/AnimationStateController.cs
@@ -7,12 +7,17 @@ public class AnimationStateController : MonoBehaviour
     public Animator animator;
     int isWalkingHash;
     int isRunningHash;
+    int isCrouchStayingHash;
+    int isCrouchWalkingHash;
     // Start is called before the first frame update
     void Start()
     {
         // улучшение производительности???? (increases perfomance)
         isWalkingHash = Animator.StringToHash("WalkBool");
         isRunningHash = Animator.StringToHash("RunBool");
+        isCrouchStayingHash = Animator.StringToHash("CrouchStayBool");
+        isCrouchWalkingHash = Animator.StringToHash("CrouchWalkBool");
+
     }
 
     // Update is called once per frame
@@ -20,26 +25,58 @@ public class AnimationStateController : MonoBehaviour
     {
         bool isRunning = animator.GetBool(isRunningHash);
         bool isWalking = animator.GetBool(isWalkingHash);
+        bool isCrouchStaying = animator.GetBool(isCrouchStayingHash);
+        bool isCrouchWalking = animator.GetBool(isCrouchWalkingHash);
         bool forwardPressed = Input.GetKey("w");
         bool runPressed = Input.GetKey("left shift");
-        if(!isWalking && forwardPressed)
+        bool crouchPressed = Input.GetKey("left ctrl");
+
+        #region Ходьба
+        if (!isWalking && forwardPressed && !crouchPressed)
         {
             animator.SetBool(isWalkingHash, true);
         }
 
-        if (isWalking && !forwardPressed) 
+        if (isWalking && (!forwardPressed || crouchPressed)) 
         { 
             animator.SetBool(isWalkingHash, false);
         }
+        #endregion
 
-        if (!isRunning && (forwardPressed && runPressed))
+        #region Бег
+        if (!isRunning && forwardPressed && runPressed && !crouchPressed)
         {
             animator.SetBool(isRunningHash, true);
         }
 
-        if(isRunning && (!forwardPressed || !runPressed))
+        if(isRunning && (!forwardPressed || !runPressed || crouchPressed))
         {
             animator.SetBool(isRunningHash, false);
         }
+        #endregion
+
+        #region Присяд
+        if (!isCrouchStaying && crouchPressed && !forwardPressed && !runPressed)
+        {
+            animator.SetBool(isCrouchStayingHash, true);
+        }
+        if(isCrouchStaying && !crouchPressed && ((forwardPressed || runPressed) || (!forwardPressed && !runPressed)))
+        {
+            animator.SetBool(isCrouchStayingHash, false);
+        }
+        #endregion
+
+        #region Ходьба в приседе
+
+        if (!isCrouchWalking && crouchPressed && (forwardPressed || (forwardPressed && runPressed)))
+        {
+            animator.SetBool(isCrouchWalkingHash, true);
+        }
+        if(isCrouchWalking && ((crouchPressed && (!forwardPressed && !runPressed)) || !crouchPressed))
+        {
+            animator.SetBool(isCrouchWalkingHash, false);
+        }
+
+        #endregion
     }
 }
