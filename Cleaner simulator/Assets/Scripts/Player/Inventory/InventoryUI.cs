2ea7fd2c826d5d62db1f5db32988db2c4e5ca355
pascal_pro_2007 <m.brednev89@gmail.com>
2024-02-14 10:52:53 +0500

    допилил инвентарь

--- Cleaner simulator/Assets/Scripts/Player/Inventory/InventoryUI.cs
+++ Cleaner simulator/Assets/Scripts/Player/Inventory/InventoryUI.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
     
-    [SerializeField] private List<Image> icons = new List<Image>();
+    [SerializeField] private List<Image> iconsOfItems = new List<Image>();
+    [SerializeField] private List<Text> counts = new List<Text>();
 
     public void UpdateUI(Inventory inventory)
     {
-        for (int i = 0; i < inventory.GetSize() && i < icons.Count; i++) 
+        for (int i = 0; i < inventory.GetSize() && i < iconsOfItems.Count; i++) 
         {
-            icons[i].sprite = inventory.GetItem(i).icon;
+            iconsOfItems[i].color = new Color(1, 1, 1, 1);
+            iconsOfItems[i].sprite = inventory.GetItem(i).icon;
+            counts[i].text = inventory.GetCount(i) > 1 ? inventory.GetCount(i).ToString() : "";
             
+        }
+        for (int i = inventory.GetSize(); i < iconsOfItems.Count; i++)
+        {
+            iconsOfItems[i].color = new Color(1, 1, 1, 0);
+            iconsOfItems[i].sprite = null;
+            counts[i].text = "";
         }
     }
 }
